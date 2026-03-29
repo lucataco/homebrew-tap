f5cc9ee045e8af54b1061e06bf97d5d1c40f712b
Luis Catacora <me@lucataco.com>
2026-03-29 11:57:23 -0400

    Add Superkeet cask

---
+++ Casks/superkeet.rb
@@ -0,0 +1,19 @@
+cask "superkeet" do
+  version "1.0.0"
+  sha256 "88cb362e0529cb0716de04a3b1bf9dcc2ef74f979d3d02ad484d72d6df5b2dca"
+
+  url "https://github.com/lucataco/superkeet/releases/download/v#{version}/Superkeet-#{version}.zip"
+  name "Superkeet"
+  desc "Local macOS voice-to-text menu bar app powered by Parakeet"
+  homepage "https://github.com/lucataco/superkeet"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Superkeet.app"
+
+  zap trash: [
+    "~/Library/Application Support/Superkeet",
+    "~/Library/Caches/com.superkeet.app",
+    "~/Library/Preferences/com.superkeet.app.plist",
+  ]
+end
