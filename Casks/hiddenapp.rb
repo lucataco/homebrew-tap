4a02fe6db24f00c6412a5da5609dc171e2f894da
Luis Catacora <me@lucataco.com>
2026-04-27 23:21:24 -0400

    Fix hiddenapp cask style

--- Casks/hiddenapp.rb
+++ Casks/hiddenapp.rb
@@ -4,14 +4,12 @@ cask "hiddenapp" do
 
   url "https://github.com/lucataco/hiddenApp/releases/download/v#{version}/HiddenApp-#{version}.zip"
   name "HiddenApp"
-  desc "macOS menu bar utility that hides other apps' status bar icons"
+  desc "Menu bar utility that hides other apps' status bar icons"
   homepage "https://github.com/lucataco/hiddenApp"
 
   depends_on macos: ">= :tahoe"
 
   app "hiddenapp.app"
 
-  zap trash: [
-    "~/Library/Preferences/com.catacolabs.hiddenapp.plist",
-  ]
+  zap trash: "~/Library/Preferences/com.catacolabs.hiddenapp.plist"
 end
