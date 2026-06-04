c5fa5407fe1cac2ce1c9664a8faa8beac1bf66f3
Luis Catacora <lucataco@Zeus.cataco>
2026-06-04 14:31:53 -0400

    Fix superkeet cask style

--- Casks/superkeet.rb
+++ Casks/superkeet.rb
@@ -4,10 +4,10 @@ cask "superkeet" do
 
   url "https://github.com/lucataco/superkeet/releases/download/v#{version}/Superkeet-#{version}.zip"
   name "Superkeet"
-  desc "Local macOS voice-to-text menu bar app powered by Parakeet"
+  desc "Local voice-to-text menu bar app powered by Parakeet"
   homepage "https://github.com/lucataco/superkeet"
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: :sonoma
 
   app "Superkeet.app"
 
