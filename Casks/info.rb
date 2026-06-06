65f4b225056d7a93371246430ed7470befa9f25c
Luis Catacora <lucataco@Zeus.cataco>
2026-06-06 14:27:56 -0400

    Update Info to v0.3.0

--- Casks/info.rb
+++ Casks/info.rb
@@ -1,6 +1,6 @@
 cask "info" do
-  version "0.2.0"
-  sha256 "e79b6019103ed4daf362ce8a74f6db8a1591aad31de50758282bdcf45c57e161"
+  version "0.3.0"
+  sha256 "b8a8d417882fd06168253ad5abd5c3def1902759ddd03b9c3deb19f4152bec8d"
 
   url "https://github.com/lucataco/Info/releases/download/v#{version}/Info-#{version}.zip"
   name "Info"
